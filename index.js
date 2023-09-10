1142ad9797509fcc43fb4401524de755545660c1
Anmolgarg18 <101702811+Anmolgarg18@users.noreply.github.com>
2023-09-10 16:29:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect("mongodb://127.0.0.1:27017/mydatabase");
+mongoose.connect("mongodb+srv://anmolgargag18:anmol@cluster0.93cq9tg.mongodb.net/?retryWrites=true");
 
 //login
 app.post("/login",(req,res)=>{
